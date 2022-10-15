434535f1a038da7105ad03e7fdcc9e2b5cfc5068
Luca Pagot <original_jpag@yahoo.it>
2022-10-15 10:37:56 +0200

    Next: Add Layout to error pages

--- brainblank_frontend/pages/404.js
+++ brainblank_frontend/pages/404.js
@@ -1,6 +1,20 @@
+import Layout from '../../components/layout'
+import { getNavigation } from '../lib/api';
 
 export default function Custom404() {
   return (
-       <h1>PAGE NOT FOUND</h1>
+    <Layout>
+      <h1>PAGE NOT FOUND</h1>
+    </Layout>
   )
+}
+
+export async function getStaticProps() {
+  const navResponse = await getNavigation(1);
+
+  return {
+    props: {
+      navigation: navResponse,
+    },
+  };
 }
--- brainblank_frontend/pages/500.js
+++ brainblank_frontend/pages/500.js
@@ -1,6 +1,20 @@
+import Layout from '../../components/layout'
+import { getNavigation } from '../lib/api';
 
 export default function Custom500() {
   return (
-       <h1>SERVER ERROR</h1>
+    <Layout>
+      <h1>SERVER ERROR</h1>
+    </Layout>
   )
+}
+
+export async function getStaticProps() {
+  const navResponse = await getNavigation(1);
+
+  return {
+    props: {
+      navigation: navResponse,
+    },
+  };
 }
